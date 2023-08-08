716424f0da1a9b945f6baf8fdb28fa03163a8090
keke <zrignshw@duck.com>
2023-08-08 18:24:30 +0800

    desktop: add sing-box(maybe move to all

--- nixos/desktop/pkgs.nix
+++ nixos/desktop/pkgs.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
 
   environment.systemPackages = with pkgs; [
     cataclysm-dda-git
@@ -7,6 +7,20 @@
     tdesktop
     pinentry-curses
   ];
+
+  #Sing-box
+  systemd.services.sing-box = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" "nss-lookup.target" ];
+    serviceConfig = {
+      ExecStart =
+        "${pkgs.sing-box}/bin/sing-box run --config /home/${config.myuser.name}/.config/sing-box/config.json";
+      ExecReload = "/run/current-system/sw/bin/pkill sing-box";
+      WorkingDirectory = "/home/${config.myuser.name}/.config/sing-box";
+      Restart = "on-failure";
+      RestartSec = 10;
+    };
+  };
 
   # GPG
   services.pcscd.enable = true;
