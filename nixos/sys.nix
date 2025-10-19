fea71ecf0cb8975f397bc6ccce47984f948580f5
keke <zrignshw@duck.com>
2025-10-20 02:16:07 +0800

    .

--- nixos/sys.nix
+++ nixos/sys.nix
@@ -11,4 +11,14 @@
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "Asia/Shanghai";
 
+  # Shell
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
 }
