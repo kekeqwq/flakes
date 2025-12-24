4bb2a6bcf52f14b199f8e006eec3a495bdf70d2a
keke <zrignshw@duck.com>
2025-12-24 21:16:42 +0800

    .

--- nixos/smb.nix
+++ nixos/smb.nix
@@ -3,14 +3,6 @@ _: {
     enable = true;
     openFirewall = true;
     settings = {
-      global = {
-        security = "user";
-        workgroup = "WORKGROUP";
-        "server string" = "keke's library";
-        "max log size" = "50";
-        "dns proxy" = false;
-        "syslog only" = true;
-      };
       public = {
         comment = "keke's library";
         path = "/home/keke/Downloads/";
@@ -24,6 +16,20 @@ _: {
         printable = "no";
         "valid users" = "keke";
       };
+      k = {
+        comment = "kkk";
+        path = "/k";
+        "force user" = "keke";
+        public = "yes";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "2777";
+        writable = "yes";
+        browseable = "yes";
+        printable = "no";
+        "valid users" = "keke";
+      };
+      
     };
   };
 }
